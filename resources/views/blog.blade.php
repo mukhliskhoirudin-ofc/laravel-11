13fdcd8f6b53b8537d5034b7d2c1df74c713473e
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-03-23 11:28:45 +0700

    feat: struktur folder

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h1>Halaman Blog</h1>
+
+    <article>
+        <h3>Judul Artikel 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis quis doloremque, adipisci maxime
+            facilis? Veritatis totam similique obcaecati iusto reiciendis, deleniti earum provident nisi pariatur
+            inventore quisquam soluta hic adipisci suscipit! Veniam saepe dolor modi, deserunt accusantium libero
+            dignissimos eius! Modi cupiditate atque commodi architecto explicabo consectetur, sunt dignissimos!</p>
+    </article>
+    <article>
+        <h3>Judul Artikel 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo quaerat pariatur ullam nobis magnam atque
+            accusamus aliquam, distinctio omnis, odit iure earum quo aut suscipit aliquid tenetur fuga cupiditate rerum.
+            Quo fugiat et ipsum culpa quisquam voluptate sequi veritatis, architecto quae delectus veniam, aliquid error
+            praesentium nihil, nesciunt ex.</p>
+    </article>
+
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li><a href="https://instagram.com/mukhliskhoirudin-ofc" target="blank">Instagram</a></li>
+        <li><a href="https://x.com/mukliskh" target="blank">X (Twitter)</a></li>
+        <li><a href="https://youtube.com/@22312008_mukhliskhoirudin" target="blank">YouTube</a></li>
+
+    </ul>
+</body>
+
+</html>
