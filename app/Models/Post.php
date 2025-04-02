765a7f1e5f2a4e3c9f84824def75d638e553b9b9
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-04-02 18:05:58 +0700

    feat: model factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    use HasFactory;
+
     //fillable = ngasih tau field yang boleh di isi
     protected $fillable = ['title', 'author', 'slug', 'body'];
 }
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence(); // Simpan title terlebih dahulu agar slugnya sama
+
+        return [
+            'title' => $title,
+            'author' => fake()->name(),
+            'slug' => Str::slug($title), // Gunakan title yang sudah disimpan
+            'body' => fake()->text(),
+        ];
+    }
+}
